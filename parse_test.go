ab781d843e41639625d549cf2d0b3a8360e85e47
midbel <brun.nicolas21@gmail.com>
2019-10-25 10:24:10 +0200

    additional test files (invalid syntax)

--- parse_test.go
+++ parse_test.go
@@ -17,15 +17,22 @@ func TestParse(t *testing.T) {
 		{File: "dates.toml", Valid: true},
 		{File: "arrays.toml", Valid: true},
 		{File: "inlines.toml", Valid: true},
+		{File: "inline2.bad.toml", Valid: false},
 		{File: "package.toml", Valid: true},
 	}
 	for _, f := range files {
 		r, err := os.Open(filepath.Join("testdata", f.File))
 		if err != nil {
 			t.Error(err)
+			continue
 		}
-		if _, err := Parse(r); err != nil && f.Valid {
-			t.Errorf("%s: unexpected error when parsing: %s", f.File, err)
+
+		switch _, err := Parse(r); {
+		case f.Valid && err != nil:
+			t.Errorf("%s: %s", f.File, err)
+		case !f.Valid && err == nil:
+			t.Errorf("%s: invalid document not detected", f.File)
 		}
+		r.Close()
 	}
 }
