20c681a4863ee00903e32403d936f753c8c56556
midbel <brun.nicolas21@gmail.com>
2020-10-10 18:22:44 +0200

    use query package to query a toml document

--- cmd/query/main.go
+++ cmd/query/main.go
@@ -1,20 +1,79 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
-  "github.com/midbel/toml"
+	"github.com/midbel/query"
+	"github.com/midbel/toml"
+)
+
+const (
+	ExitBadQuery int = iota + 1
+	ExitBadDoc
+	ExitEmpty
 )
 
 func main() {
-  flag.Parse()
+	path := flag.Bool("p", false, "print path")
+	flag.Parse()
 
-  doc := make(map[string]interface{})
-  if err := toml.DecodeFile(flag.Arg(0), &doc); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
-  fmt.Println(doc)
+	q, err := query.Parse(flag.Arg(0))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, flag.Arg(0), err)
+		os.Exit(ExitBadQuery)
+	}
+
+	doc := make(map[string]interface{})
+	if err := toml.DecodeFile(flag.Arg(1), &doc); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(ExitBadDoc)
+	}
+	ifi, err := q.Select(doc)
+	switch {
+	case err != nil:
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(ExitBadQuery)
+	case ifi == nil:
+		os.Exit(ExitEmpty)
+	default:
+	}
+	var (
+		root  = filepath.Base(flag.Arg(1))
+		print = nokey
+	)
+	if *path {
+		print = withkey
+	}
+	printResults(strings.TrimSuffix(root, ".toml"), ifi, print)
+}
+
+func nokey(_ string, value interface{}) {
+	fmt.Println(value)
+}
+
+func withkey(key string, value interface{}) {
+	fmt.Printf("%s = %v\n", key, value)
+}
+
+func printResults(key string, value interface{}, print func(string, interface{})) {
+	switch ifi := value.(type) {
+	case []interface{}:
+		if len(ifi) == 1 {
+			printResults(key, ifi[0], print)
+			return
+		}
+		for j, i := range ifi {
+			printResults(fmt.Sprintf("%s.%d", key, j), i, print)
+		}
+	case map[string]interface{}:
+		for k, v := range ifi {
+			printResults(fmt.Sprintf("%s.%s", key, k), v, print)
+		}
+	default:
+		print(key, ifi)
+	}
 }
