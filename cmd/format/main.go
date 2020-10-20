45cc22c5858033736d25a7698e4eb70ce74d3a20
midbel <brun.nicolas21@gmail.com>
2020-10-20 16:14:21 +0200

    overwrite document

--- cmd/format/main.go
+++ cmd/format/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/midbel/toml"
@@ -10,7 +12,7 @@ import (
 
 func main() {
 	var (
-		// overwrite = flag.Bool("w", false, "overwrite document")
+		overwrite = flag.Bool("w", false, "overwrite document")
 		// general option
 		keep  = flag.Bool("k", false, "keep empty table(s)")
 		nest  = flag.Bool("n", false, "nest sub table(s)")
@@ -29,15 +31,33 @@ func main() {
 		toml.WithNest(*nest),
 		toml.WithFloat(*float),
 		toml.WithNumber(*decimal),
+		toml.WithComment(true),
 	}
 	for _, a := range flag.Args() {
-		ft, err := toml.NewFormatter(a, rules...)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-		if err := ft.Format(os.Stdout); err != nil {
+		if err := formatDocument(a, *overwrite, rules); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
+}
+
+func formatDocument(doc string, overwrite bool, rules []toml.FormatRule) error {
+	ft, err := toml.NewFormatter(doc, rules...)
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	if err := ft.Format(&buf); err != nil {
+		return err
+	}
+	out := os.Stdout
+	if overwrite {
+		w, err := os.Create(doc)
+		if err != nil {
+			return err
+		}
+		defer w.Close()
+		out = w
+	}
+	_, err = io.Copy(out, &buf)
+	return err
 }
