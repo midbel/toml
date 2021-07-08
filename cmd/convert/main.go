32b345d2126a531cfab6494c1b827eb5c0f962f2
midbel <brun.nicolas21@gmail.com>
2021-07-08 10:20:40 +0200

    helper command to convert toml doc to json

---
+++ cmd/convert/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/midbel/toml"
+)
+
+func main() {
+	flag.Parse()
+
+	w, err := os.Create(getFile(flag.Arg(0)))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	defer w.Close()
+
+  if err := save(w, flag.Arg(0)); err != nil {
+    fmt.Fprintln(os.Stderr, err)
+    os.Exit(1)
+  }
+}
+
+func save(w io.Writer, file string) error {
+  var in interface{}
+  if err := toml.DecodeFile(file, &in); err != nil {
+    return err
+  }
+  e := json.NewEncoder(w)
+  e.SetIndent("", "  ")
+  return e.Encode(in)
+}
+
+func getFile(file string) string {
+  file = strings.TrimRight(filepath.Ext(file))
+  return file + ".json"
+}
